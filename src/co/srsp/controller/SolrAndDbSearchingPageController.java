98f7e7029b4de56227a4aea23c9f5881db555513
newellpj <pauljamesnewell@gmail.com>
2016-09-29 20:22:36 +1000

    sdfsdf

--- src/co/srsp/controller/SolrAndDbSearchingPageController.java
+++ src/co/srsp/controller/SolrAndDbSearchingPageController.java
@@ -217,16 +217,19 @@ public class SolrAndDbSearchingPageController {
 					log.info("bookRev.getStarRating() : "+bookRev.getStarRating());
 					
 					if(bookRev.getStarRating() != null){
-						for(int i = 0; bookRev.getStarRating() > i ;i++){
-							starRatingsHTMLBuffer.append("<span style='color:'#f70;text-shadow:1px 1px #c60, 2px 2px #940, .1em .1em .2em rgba(0,0,0,.5)'");
-							starRatingsHTMLBuffer.append("'\2605'");
-							starRatingsHTMLBuffer.append("</span></br>");
-						}
+						//for(int i = 0; bookRev.getStarRating() > i ;i++){
+							starRatingsHTMLBuffer.append("<div class=");
+							starRatingsHTMLBuffer.append("stars"+bookRev.getStarRating());
+							starRatingsHTMLBuffer.append("</div>");
+						//}
 					}
 				
 					//starRatingsHTMLBuffer.append("</div>");
 				log.info("starRatingsHTMLBuffer : "+starRatingsHTMLBuffer.toString());
-					list.add(starRatingsHTMLBuffer.toString()+bookRev.getReviewText()+" - <b>reviewed by "+bookRev.getReviewersUsername()+"</b>");
+				
+					String reviewsFormattedString = starRatingsHTMLBuffer.toString()+bookRev.getReviewText()+" - <b>reviewed by "+bookRev.getReviewersUsername()+"</b>";
+					log.info("reviewsFormattedString : "+reviewsFormattedString);
+					list.add(reviewsFormattedString);
 				}
 			}
 			
