6a5b330fca56693b1cbec20fd2b6f712c734828b
newellpj <pauljamesnewell@gmail.com>
2016-09-30 01:13:11 +1000

    sdfds

---
+++ src/co/srsp/session/models/ReviewBookSessionBean.java
@@ -0,0 +1,10 @@
+package co.srsp.session.models;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="session")
+public class ReviewBookSessionBean {
+
+}
