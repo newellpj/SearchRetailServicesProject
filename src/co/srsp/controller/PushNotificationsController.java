2d2cd788f761339f97ccda0799248eb3d05a5d2f
newellpj <pauljamesnewell@gmail.com>
2016-10-12 12:07:24 +1100

    ds

--- src/co/srsp/controller/PushNotificationsController.java
+++ src/co/srsp/controller/PushNotificationsController.java
@@ -3,6 +3,7 @@ package co.srsp.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +14,15 @@ import co.srsp.viewmodel.BookReviewsModel;
 @Controller
 public class PushNotificationsController {
 
+	private final static Logger log = Logger.getLogger(PushNotificationsController.class); 
+	
 	@RequestMapping(value = { "/addSubscription"}, method = RequestMethod.GET)
 	public @ResponseBody BookReviewsModel addSubscription(HttpServletRequest request, HttpServletResponse response){
+		log.info("addSubscription !!!!");
+		log.info("statusType :: "+request.getParameter("statusType"));
+		log.info("username :: "+request.getParameter("username"));
+		log.info("endpoint :: "+request.getParameter("endpoint"));
+		log.info("key :: "+request.getParameter("key"));
 		return null;
 	}
 	
