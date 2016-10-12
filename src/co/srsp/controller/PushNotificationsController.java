3ed19ed839db805ef733858bd0433419507162f2
newellpj <pauljamesnewell@gmail.com>
2016-10-13 05:32:14 +1100

    notifications

--- src/co/srsp/controller/PushNotificationsController.java
+++ src/co/srsp/controller/PushNotificationsController.java
@@ -1,6 +1,8 @@
 package co.srsp.controller;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -8,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
@@ -17,14 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.Result;
+import com.google.android.gcm.server.Sender;
+
 import co.srsp.config.ConfigHandler;
 import co.srsp.hibernate.orm.NotificationSubscribers;
 import co.srsp.service.NotificationsService;
 import co.srsp.viewmodel.BookReviewsModel;
 
+
 @Controller
 public class PushNotificationsController {
 
+
+	
 	private final static Logger log = Logger.getLogger(PushNotificationsController.class); 
 	
 	//method for storing 
@@ -70,69 +78,107 @@ public class PushNotificationsController {
 		return returnArray;
 	}
 	
+	private class FCMSender extends Sender {
+
+	    public FCMSender(String key) {
+	        super(key);
+	    }
+
+	    @Override
+	    protected HttpURLConnection getConnection(String url) throws IOException {
+	        String fcmUrl = "https://fcm.googleapis.com/fcm/send";
+	        return (HttpURLConnection) new URL(fcmUrl).openConnection();
+	    }
+	}
+	
 	//TODO
 	//this will generate notification for sake of testing - normally have a poller that checks for updates to notify subscribers
 	//event to notify and then retrieve list of subscribers
 	public void sendNotificationsToSubscribers(){
 		//Server key:	AIzaSyC_twu_3RmawPiC6hL4h5KTAb0sz33HaNQ
-		//Sender ID:	954512341945
+		//Sen
 		
+		
+//park this for now
 		String rootURL = ConfigHandler.getInstance().readApplicationProperty("rootFirebaseURL");
 		String key = ConfigHandler.getInstance().readApplicationProperty("firebaseServerKey");
+		
+		  try {
+              Sender sender = new FCMSender(key);
+              Message message = new Message.Builder()
+                                .collapseKey("message")
+                                .timeToLive(3)
+                                .delayWhileIdle(true)
+                                .addData("message", "Test Notification from Java application")
+                                .build();  
+
+            //   Use the same token(or registration id) that was earlier
+             //  used to send the message to the client directly from
+             //  Firebase Console's Notification tab.
+                                
+            //registration IDs.
+                                
+              Result result = sender.send(message,
+          "dRzT9QLVFQo:APA91bGNAhAXoSFr0uuvrMKp5eWhU5WzOt7TwwrP_qrIHZmPiuROG4fNDhfpdU3Ri3dCwMOM_qonacmkc0Vw-dIqoUkacHDQJsjyxEjBqg6r-aAof9uTcB_58Y931D6HZjmXi0l0z4AQ",
+                  1);
+              System.out.println("Result: " + result.toString());
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+		
+		
+		
 		
 		//String ContentType = "application/json"; 
 		rootURL += "";
 	
 		
-		try{
-			URL obj = new URL(rootURL);
-			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-			con.setRequestMethod("POST");
-			
-			con.setDoOutput(true);
-			con.setDoInput(true);
-			con.setRequestProperty("User-Agent", "Mozilla/5.0");
-			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setRequestProperty("Accept", "application/json");
-			//con.set
-			//https://android.googleapis.com/gcm/send/
-			JSONObject parent = new JSONObject();
-			JSONObject cred   = new JSONObject();
-			JSONObject subIDs = new JSONObject();
-			
-			
-			String[] subIDArray = new String[2]; //TODO 2 for now just to test
-			subIDArray[0] = "dRzT9QLVFQo:APA91bGNAhAXoSFr0uuvrMKp5eWhU5WzOt7TwwrP_qrIHZmPiuROG4fNDhfpdU3Ri3dCwMOM_qonacmkc0Vw-dIqoUkacHDQJsjyxEjBqg6r-aAof9uTcB_58Y931D6HZjmXi0l0z4AQ";
-			subIDArray[1] = "dRzT9QLVFQo:APA91bGNAhAXoSFr0uuvrMKp5eWhU5WzOt7TwwrP_qrIHZmPiuROG4fNDhfpdU3Ri3dCwMOM_qonacmkc0Vw-dIqoUkacHDQJsjyxEjBqg6r-aAof9uTcB_58Y931D6HZjmXi0l0z4AQ";
-			
-			subIDs.put("registration_ids", subIDArray);
-			
-			cred.put("Authorization:", " key="+key);
-			cred.put("Authorization:", " key="+key);
-			
-			
-			String urlParameters = "";
-			parent.put("cred", cred.toString());
-
-			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			wr.writeBytes(urlParameters);
-			wr.flush();
-			wr.close();
-			
-			int responseCode = con.getResponseCode();
-			log.info("\nSending 'POST' request to URL : " + rootURL);
-			log.info("Post parameters : " + urlParameters);
-			log.info("Response Code : " + responseCode);
-			
-			
-			HttpClient client = new DefaultHttpClient();
-			//HttpPost post = new HttpPost(url);
-			
-		}catch(Exception e){
-			e.printStackTrace();
-			log.error(e.getMessage());
-		}
+//		try{
+//			URL obj = new URL(rootURL);
+//			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+//			con.setRequestMethod("POST");
+//			
+//			con.setDoOutput(true);
+//			con.setDoInput(true);
+//			con.setRequestProperty("User-Agent", "Mozilla/5.0");
+//			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+//			con.setRequestProperty("Content-Type", "application/json");
+//			con.setRequestProperty("Accept", "application/json");
+//			con.setRequestProperty("Authorization:", key="+key");
+//		
+//			JSONObject parent = new JSONObject();
+//			JSONObject cred   = new JSONObject();
+//			JSONObject subIDs = new JSONObject();
+//			
+//			
+//			String[] subIDArray = new String[2]; //TODO 2 for now just to test
+//			subIDArray[0] = "dRzT9QLVFQo:APA91bGNAhAXoSFr0uuvrMKp5eWhU5WzOt7TwwrP_qrIHZmPiuROG4fNDhfpdU3Ri3dCwMOM_qonacmkc0Vw-dIqoUkacHDQJsjyxEjBqg6r-aAof9uTcB_58Y931D6HZjmXi0l0z4AQ";
+//			subIDArray[1] = "dRzT9QLVFQo:APA91bGNAhAXoSFr0uuvrMKp5eWhU5WzOt7TwwrP_qrIHZmPiuROG4fNDhfpdU3Ri3dCwMOM_qonacmkc0Vw-dIqoUkacHDQJsjyxEjBqg6r-aAof9uTcB_58Y931D6HZjmXi0l0z4AQ";
+//			
+//			subIDs.put("registration_ids", subIDArray);
+//			
+//
+//			String urlParameters = "";
+//			parent.put("cred", cred.toString());
+//
+//			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+//			wr.writeBytes(urlParameters);
+//			wr.flush();
+//			wr.close();
+//			
+//			int responseCode = con.getResponseCode();
+//			log.info("\nSending 'POST' request to URL : " + rootURL);
+//			log.info("Post parameters : " + urlParameters);
+//			log.info("Response Code : " + responseCode);
+//			
+//			
+//			HttpClient client = new DefaultHttpClient();
+//	
+//			
+//		}catch(Exception e){
+//			e.printStackTrace();
+//			log.error(e.getMessage());
+//		}
 		
 		
 		//https://android.googleapis.com/gcm/send/e7ze8FD3Itg:APA91bHG8GvyNo24jQPksssRQkqZyF2cGzbJGmJpfgn8qgzDCJ07eVFOoV7T2S96jdj6qKnWUfwQJhO9HXSYc78PkgkuZDDuQoibEzavWii5JQF5R-VPSJvUQ_9anIqaugjTVHDgCzaw
